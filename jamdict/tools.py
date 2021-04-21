f669eb3de5d3b88da5b66146977e87990b9ffb4e
Le Tuan Anh <tuananh.ke@gmail.com>
2021-04-21 18:20:15 +0800

    make config file customizable

--- jamdict/tools.py
+++ jamdict/tools.py
@@ -50,12 +50,6 @@ import jamdict
 # Configuration
 # -------------------------------------------------------------------------------
 
-
-JMD_XML = jamdict.config.get_file('JMDICT_XML')
-KD2_XML = jamdict.config.get_file('KD2_XML')
-JMNE_XML = jamdict.config.get_file('JMNEDICT_XML')
-JMD_DB = jamdict.config.get_file('JAMDICT_DB')
-
 if os.path.isfile('logging.json'):
     setup_logging('logging.json', 'logs')
 else:
@@ -73,20 +67,24 @@ def getLogger():
 def get_jam(cli, args):
     if not args.jdb:
         args.jdb = None
+    if args.config:
+        jamdict.config.read_config(args.config)
     if args.kd2 or args.jmne:
         cli.logger.warning("Jamdict database location: {}".format(args.jdb))
         cli.logger.warning("Kanjidic2 database location: {}".format(args.kd2))
         jmd = jamdict.Jamdict(db_file=args.jdb, kd2_file=args.kd2,
-                      jmd_xml_file=args.jmdxml, kd2_xml_file=args.kd2xml,
-                      jmnedict_file=args.jmne, jmnedict_xml_file=args.jmnexml)
+                              jmd_xml_file=args.jmdxml,
+                              kd2_xml_file=args.kd2xml,
+                              jmnedict_file=args.jmne,
+                              jmnedict_xml_file=args.jmnexml)
     else:
         cli.logger.debug("Using the same database for both JMDict and Kanjidic2")
         jmd = jamdict.Jamdict(db_file=args.jdb,
-                      kd2_file=args.jdb,
-                      jmnedict_file=args.jdb,
-                      jmd_xml_file=args.jmdxml,
-                      kd2_xml_file=args.kd2xml,
-                      jmnedict_xml_file=args.jmnexml)
+                              kd2_file=args.jdb,
+                              jmnedict_file=args.jdb,
+                              jmd_xml_file=args.jmdxml,
+                              kd2_xml_file=args.kd2xml,
+                              jmnedict_xml_file=args.jmnexml)
     if jmd.kd2 is None:
         cli.logger.warning("Kanjidic2 database could not be found")
     return jmd
@@ -98,10 +96,6 @@ def import_data(cli, args):
     t = Timer(report=rp)
     db_loc = os.path.abspath(os.path.expanduser(args.jdb))
     show_info(cli, args)
-    # rp.print("Jamdict DB location        : {}".format(db_loc))
-    # rp.print("JMDict XML file location   : {}".format(args.jmdxml))
-    # rp.print("Kanjidic2 XML file location: {}".format(args.kd2xml))
-    # rp.print("JMnedict XML file location : {}".format(args.jmnexml))
     jam = get_jam(cli, args)
     if args and (args.jdb or args.kd2):
         if os.path.isfile(db_loc):
@@ -181,12 +175,14 @@ def lookup(cli, args):
                 dump_result(results, report=report)
     else:
         getLogger().warning(f"Jamdict database is not available.\nThere are 3 ways to install data: \n    1) install jamdict_data via PyPI using `pip install jamdict_data` \n    2) download prebuilt dictionary database file from: {jamdict.__url__}, \n    3) or build your own database file from XML source files.")
-        
 
 
 def file_status(file_path):
-    real_path = os.path.abspath(os.path.expanduser(file_path))
-    return '[NOT FOUND]' if not os.path.isfile(real_path) else '[OK]'
+    if file_path:
+        real_path = os.path.abspath(os.path.expanduser(file_path))
+        if os.path.isfile(real_path):
+            return '[OK]'
+    return '[NOT FOUND]'
 
 
 def hello_jamdict(cli, args):
@@ -202,23 +198,34 @@ def hello_jamdict(cli, args):
 def show_info(cli, args):
     ''' Show jamdict configuration (data folder, configuration file location, etc.) '''
     output = TextReport(args.output) if 'output' in args else TextReport()
+    if args.config:
+        jamdict.config.read_config(args.config)
     output.print("Jamdict " + jamdict.version_info.__version__)
     output.print(jamdict.version_info.__description__)
+    jam = get_jam(cli, args)
     output.header("Basic configuration")
-    output.print(f"JAMDICT_HOME             : {jamdict.config.home_dir()}")
-    output.print(f"jamdict_data availability: {jamdict.util._JAMDICT_DATA_AVAILABLE}")
-    _config_path = jamdict.config._get_config_manager().locate_config()
+    jamdict_home = jamdict.config.home_dir()
+    if not os.path.isdir(jamdict_home):
+        jamdict_home += " [Missing]"
+    output.print(f"JAMDICT_HOME        : {jamdict_home}")
+    data_pkg = 'Installed' if jamdict.util._JAMDICT_DATA_AVAILABLE else 'Not installed'
+    output.print(f"jamdict_data package: {data_pkg}")
+    if args.config:
+        _config_path = args.config + " [Custom]"
+        if not os.path.isfile(args.config):
+            _config_path += " [Missing]"
+    else:
+        _config_path = jamdict.config._get_config_manager().locate_config()
     if not _config_path:
         _config_path = "Not available.\n     Run `python3 -m jamdict config` to create configuration file if needed."
-    output.print(f"Config file location     : {_config_path}")
+    output.print(f"Config file location: {_config_path}")
 
-    output.header("Custom data files")
-    output.print("Jamdict DB location: {} - {}".format(args.jdb, file_status(args.jdb)))
-    output.print("JMDict XML file    : {} - {}".format(args.jmdxml, file_status(args.jmdxml)))
-    output.print("KanjiDic2 XML file : {} - {}".format(args.kd2xml, file_status(args.kd2xml)))
-    output.print("JMnedict XML file : {} - {}".format(args.jmnexml, file_status(args.jmnexml)))
+    output.header("Data files")
+    output.print(f"Jamdict DB location: {jam.db_file} - {file_status(jam.db_file)}")
+    output.print(f"JMDict XML file    : {jam.jmd_xml_file} - {file_status(jam.jmd_xml_file)}")
+    output.print(f"KanjiDic2 XML file : {jam.kd2_xml_file} - {file_status(jam.kd2_xml_file)}")
+    output.print(f"JMnedict XML file : {jam.jmnedict_xml_file} - {file_status(jam.jmnedict_xml_file)}")
 
-    jam = get_jam(cli, args)
     if jam.ready:
         output.header("Jamdict database metadata")
         try:
@@ -241,7 +248,10 @@ def show_version(cli, args):
 
 def config_jamdict(cli, args):
     ''' Create configuration file '''
-    jamdict.config._ensure_config()
+    if args.config:
+        jamdict.config._ensure_config(args.config)
+    else:
+        jamdict.config._ensure_config()
     show_info(cli, args)
 
 
@@ -250,10 +260,11 @@ def config_jamdict(cli, args):
 # -------------------------------------------------------------------------------
 
 def add_data_config(parser):
-    parser.add_argument('-j', '--jmdxml', help='Path to JMdict XML file', default=JMD_XML)
-    parser.add_argument('-k', '--kd2xml', help='Path to KanjiDic2 XML file', default=KD2_XML)
-    parser.add_argument('-e', '--jmnexml', help='Path to JMnedict XML file', default=JMNE_XML)
-    parser.add_argument('-J', '--jdb', help='Path to JMDict SQLite file', default=JMD_DB)
+    parser.add_argument('-c', '--config', help='Path to Jamdict config file (i.e. ~/.jamdict/config.json)', default=None)
+    parser.add_argument('-J', '--jdb', help='Path to JMDict SQLite file', default=None)
+    parser.add_argument('-j', '--jmdxml', help='Path to JMdict XML file', default=None)
+    parser.add_argument('-k', '--kd2xml', help='Path to KanjiDic2 XML file', default=None)
+    parser.add_argument('-e', '--jmnexml', help='Path to JMnedict XML file', default=None)
     parser.add_argument('-K', '--kd2', help='Path to KanjiDic2 SQLite file', default=None)
     parser.add_argument('-E', '--jmne', help='Path to JMnedict SQLite file', default=None)
 
@@ -266,7 +277,7 @@ def main():
 
     # import task
     import_task = app.add_task('import', func=import_data)
-    add_data_config(import_task)    
+    add_data_config(import_task)
 
     # show info
     info_task = app.add_task('info', func=show_info)
@@ -284,7 +295,7 @@ def main():
     # hello
     hello_task = app.add_task('hello', func=hello_jamdict)
     add_data_config(hello_task)
-    
+
     # look up task
     lookup_task = app.add_task('lookup', func=lookup)
     lookup_task.add_argument('query', help='kanji/kana')
