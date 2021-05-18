0dc828e57398b38e287d6bae50238bf56649f929
Le Tuan Anh <tuananh.ke@gmail.com>
2021-05-18 22:06:22 +0800

    use abspath to setup test log

--- test/__init__.py
+++ test/__init__.py
@@ -10,7 +10,6 @@
 import os
 from chirptext.cli import setup_logging
 
-TEST_DIR = os.path.dirname(__file__)
+TEST_DIR = os.path.abspath(os.path.dirname(__file__))
 TEST_DATA = os.path.join(TEST_DIR, 'data')
-
 setup_logging(os.path.join(TEST_DIR, 'logging.json'), os.path.join(TEST_DIR, 'logs'))
