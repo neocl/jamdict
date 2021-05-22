61a23648d396c480c50687e7b9864cb61a3346fc
Le Tuan Anh <tuananh.ke@gmail.com>
2021-05-22 23:05:18 +0800

    add all_ne_type method

--- jamdict/jmnedict_sqlite.py
+++ jamdict/jmnedict_sqlite.py
@@ -76,6 +76,12 @@ class JMNEDictSQLite(JMNEDictSchema):
     def __init__(self, db_path, *args, **kwargs):
         super().__init__(db_path, *args, **kwargs)
 
+    def all_ne_type(self, ctx=None):
+        if ctx is None:
+            return self.all_ne_type(ctx=self.ctx())
+        else:
+            return [x['text'] for x in ctx.select("SELECT DISTINCT text FROM NETransType")]
+        
     def search_ne(self, query, ctx=None, **kwargs):
         # ensure context
         if ctx is None:
