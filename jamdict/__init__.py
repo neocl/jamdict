c910bea3a12d2f560f219b259fb7dde4aa0af59b
Le Tuan Anh <tuananh.ke@gmail.com>
2021-05-22 17:31:42 +0800

    easier version config

--- jamdict/__init__.py
+++ jamdict/__init__.py
@@ -45,7 +45,7 @@ References:
 from . import __version__ as version_info
 from .__version__ import __author__, __email__, __copyright__, __maintainer__
 from .__version__ import __credits__, __license__, __description__, __url__
-from .__version__ import __version_major__, __version_long__, __version__, __status__
+from .__version__ import __version__, __version_long__, __status__
 
 from .jmdict_sqlite import JMDictSQLite
 from .kanjidic2_sqlite import KanjiDic2SQLite
