e85cf361c6d021f3e0c509e3eb950249c4df3bf9
Le Tuan Anh <tuananh.ke@gmail.com>
2021-05-24 23:26:03 +0800

    use another cursor to loop through idseqs in iter search

--- jamdict/jmdict_sqlite.py
+++ jamdict/jmdict_sqlite.py
@@ -145,7 +145,7 @@ class JMDictSQLite(JMDictSchema):
         # else (a context is provided)
         logging.getLogger(__name__).debug(f"Search query: {where} -- Params: {params}")
         return where, params
-        
+
     def search(self, query, ctx=None, pos=None, **kwargs):
         # ensure context
         if ctx is None:
@@ -153,9 +153,8 @@ class JMDictSQLite(JMDictSchema):
                 return self.search(query, ctx=ctx, pos=pos)
         where, params = self._build_search_query(query, pos=pos)
         where.insert(0, 'SELECT idseq FROM Entry WHERE ')
-        idseqs = tuple(x['idseq'] for x in ctx.execute(' '.join(where), params))
         entries = []
-        for idseq in idseqs:
+        for (idseq,) in ctx.conn.cursor().execute(' '.join(where), params):
             entries.append(self.get_entry(idseq, ctx=ctx))
         return entries
 
